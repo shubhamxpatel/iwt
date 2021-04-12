44e82f053fb6ecf046c60c573cbc9fae6ef6b9d1
shubhamxpatel <sp1651999@gmai.com>
2021-04-13 01:29:57 +0530

    second1

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,15 @@
 var express = require('express');
 var router = express.Router();
 const fetch = require('node-fetch')
-var re = /<a[^>]*>[^(\/a)]*<\/a>/mgi
+var re = /href="(.*?)"/mgi
 async function fun(res, s) {
 
 
-    const response = await fetch(s);
+    const response = await fetch(`https://www.google.com/search?q=${s.split(" ").join("+")}`);
     await response.text().then((text) => {
-        let p = text.match(re).join('$')
-            //console.log(p)
+        let p = text.match(re)
+
+        //console.log(p)
 
         //console.log(p)
 
@@ -23,7 +24,7 @@ async function fun(res, s) {
 /* GET users listing. */
 router.get('/', function(req, res, next) {
     if (req.query.link === undefined) {
-        res.render('webscrap', { data: "" })
+        res.render('webscrap', { data: [] })
     } else {
         fun(res, req.query.link)
     }
